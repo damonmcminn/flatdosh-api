2cfc498de052fb7c7513c0e0bf0ba4c1f0ef7b1c
Damon McMinn <damonrmcminn@gmail.com>
2015-04-22 14:39:31 +0100

    'group' field required

--- es6/user/model.js
+++ es6/user/model.js
@@ -1,6 +1,6 @@
 import db from '../db';
 import createSchema from 'schema';
-import {hash} from '../auth';
+import hash from '../auth/hash';
 
 const r = db.r;
 const conn = db.conn;
@@ -21,17 +21,22 @@ let validate = createSchema([
   {
     field: 'password',
     type: String,
-    require: true
+    required: true
+  },
+  {
+    field: 'group',
+    type: String,
+    required: true
   }
 ]);
 
 
 function insert(user) {
   
-  let {email, name, password} = user;
+  let {email, name, password, group} = user;
 
   return hash(password).then(hashed => {
-    let doc = {id: email, name, password: hashed};
+    let doc = {id: email, name, password: hashed, group};
     return users.insert(doc).run(conn);
   });
 };
@@ -41,4 +46,10 @@ function all() {
     .then(db.toArray);
 }
 
-export default {insert, all, validate};
+function get(email) {
+
+  return users.get(email).run(conn);
+
+}
+
+export default {insert, all, validate, get};
