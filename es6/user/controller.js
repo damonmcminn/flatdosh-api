33be94117a78b9f62aa0125f08ed677701df8740
Damon McMinn <damonrmcminn@gmail.com>
2015-04-22 14:41:14 +0100

    Export two routers (login, register)
    Also added comments re: required 'group' field and temporarily get group from config

--- es6/user/controller.js
+++ es6/user/controller.js
@@ -1,10 +1,10 @@
 import {Router} from 'express';
 import User from './model';
 import {isError} from 'js-type-check';
+import {group} from 'parse-config';
 
 const router = Router();
-
-export default router;
+const register = Router();
 
 function findAll(req, res, next) {
 
@@ -16,6 +16,14 @@ function findAll(req, res, next) {
 
 function insert(req, res, next) {
 
+  // logic
+  // same as register page, but with extra field for group name
+  // create group, with primary contact email (group consensus allows change)
+  // create user
+  // return token
+  // carry on
+  req.body.group = group;
+
   let user = User.validate(req.body);
 
   return isError(user) ? next(user) : 
@@ -25,5 +33,21 @@ function insert(req, res, next) {
     .catch(err => next(err));
 }
 
-router.get('/', findAll);
-router.post('/', insert);
+function find(req, res, next) {
+
+  let email = req.params.email;
+
+  User.get(email)
+    .then(user => {
+      user ? res.json(user) : res.status(404).json({message: 'User not found'});
+    })
+    .catch(err => next(err));
+
+}
+
+//router.get('/', findAll);
+
+login.get('/:email', find);
+register.post('/', insert);
+
+export default {login, register};
