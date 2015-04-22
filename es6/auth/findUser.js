9ecf100e2233139bbdf2b409bd79b514c921ba3e
Damon McMinn <damonrmcminn@gmail.com>
2015-04-22 14:30:27 +0100

    Implemented findUser

--- es6/auth/findUser.js
+++ es6/auth/findUser.js
@@ -1,12 +1,43 @@
 import authUtil from 'auth-utilities';
 import auth from 'auth-middleware';
 import ErrorFactory from 'simple-error-factory';
+import log from '../log';
+import User from '../user/model';
+import hash from './hash';
 
 const parseBasic = authUtil.parseHeader('basic');
-
+const AuthError = ErrorFactory('auth');
 
 export default function findUser(req) {
 
-  return Promise.reject(ErrorFactory('rejection')());
+  const TODAY = Date.now();
+  const THREE_MONTHS = TODAY + (365*24*60*60*1000)/4;
+  let header = req.headers.authorization;
+
+  // undefined if false
+  let parsed = parseBasic(header);
+  let {user, password} = parsed;
+
+  // logging base64 encoded password here... possibly
+  log.info({login: user || header});
+
+  if (!parsed) return Promise.reject(AuthError(`Couldn't parse: ${header}`));
+
+  return User.get(user).then(result => {
+
+    let payload = {
+      user: result.id,
+      exp: THREE_MONTHS,
+      name: result.name,
+      group: result.group
+    };
+
+    let hash = result.password;
+    let plain = password;
+
+    return Promise.resolve({payload, hash, plain});
+
+  })
+  .catch(err => Promise.reject(err));
 
 }
