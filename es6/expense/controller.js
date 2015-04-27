9d395f3536d208ef9894fc5672742bab3be3663b
Damon McMinn <damonrmcminn@gmail.com>
2015-04-27 17:35:42 +0100

    Split expense if user shares money with another

--- es6/expense/controller.js
+++ es6/expense/controller.js
@@ -18,11 +18,24 @@ function findAll(req, res, next) {
 
 function insert(req, res, next) {
 
-  req.body.timestamp = new Date();
-  let expense = Expense.validate(req.body);
+  let people = [req.user.user, req.user.shared].filter(email => email);
+  let timestamp = new Date();
+  let amount = req.body.amount/people.length;
 
-  return isError(expense) ? next(expense) : 
-    Expense.insert(expense).then(result => {
+  let expenses = people.map(email => {
+    return {
+      amount,
+      description: req.body.description,
+      timestamp,
+      email,
+      group: req.user.group
+    }
+  });
+
+  console.log(expenses);
+
+  return isError(Expense.validate(expenses[0])) ? next(expenses[0]) : 
+    Expense.insert(expenses).then(result => {
       return result.errors === 0 ? res.json(result) : next(result);
     })
     .catch(err => next(err));
