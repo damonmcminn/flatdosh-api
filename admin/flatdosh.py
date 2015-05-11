9447482db911d52ee0484355a19758fad2cf8c0a
Damon McMinn <damonrmcminn@gmail.com>
2015-05-11 08:05:06 +0100

    Add members; drop user

--- admin/flatdosh.py
+++ admin/flatdosh.py
@@ -32,7 +32,7 @@ def add_members(id, xs):
 def groups():
     for group, id in g.iteritems():
         print ' * ' + group
-        #print '   ' + id
+        #print json.dumps(db.groups.get(id)['members'].run(), indent=2)
         for member in db.groups.get(id)['members'].run():
             print '    - ' + member
 
@@ -57,6 +57,9 @@ parser = argparse.ArgumentParser(description='flatdosh admin')
 parser.add_argument('-u', '--users', action='store_true')
 parser.add_argument('-g', '--groups', action='store_true')
 parser.add_argument('-H', '--history')
+parser.add_argument('--add')
+parser.add_argument('-m', '--members')
+parser.add_argument('--kill')
 
 args = parser.parse_args()
 
@@ -68,3 +71,18 @@ if args.groups:
 
 if args.history:
     history(args.history)
+
+if args.add and args.members:
+    members = args.members.split(',')
+    add_members(g[args.add], members)
+    groups()
+
+if args.kill:
+    confirmed = raw_input('Are you sure? (y/N) ')
+    if confirmed.lower() == 'y':
+        result = db.users.get(args.kill).delete().run()
+        if result['deleted']:
+            print 'Deleted: ' + args.kill
+        else:
+            print 'Failure:'
+            print json.dumps(result, indent=2)
